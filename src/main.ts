929c406c3b6b63d29c17f24805d4921c961ce94e
JosephOlu <161716305+JosephOluOlofinte@users.noreply.github.com>
2024-11-02 06:09:42 +0100

    update

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,9 @@ import testFunc from './todoapp/todo'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-    <h1>Welcome to My Todo App! </h1>
+    <h1 class="test">Welcome to My Todo App! </h1>
     <p id='godhelpme'></p>
   </div>
+  
 `
 testFunc(document.querySelector<HTMLParagraphElement>('#godhelpme')!)
